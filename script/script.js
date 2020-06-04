b8db333388a2ed1f7f4b0d70e7eb18dfe03977d9
Tetrapleg <rufmama@yandex.ru>
2020-06-04 16:43:37 +0300

    Домашнее задание первого урока

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
 
+let money = 50; 
+
+const income = "Родившиеся до ";
+
+let addExpenses = false;
+
+let deposit = '20 $'; 
+
+const mission = null; 
+
+let period = {a: 'day', b: 'monse', c: 'year'};
+
+// alert ('Первый урок!!');
+
+console.log(income + period.a + ' ' + period['b'] + ' ' + period.c + ' получают скидку ' + money + ' %' + ' ' + 'и ваш платёж составляет ' + deposit);
+
+console.log(period);
+
+period.a = 19;
+period.b = 'may';
+period.c = '1999 г'
+
+console.log(income + period.a + ' ' + period['b'] + ' ' + period.c + ' получают скидку ' + money + ' %' + ' ' + 'и ваш платёж составляет ' + deposit);
+
